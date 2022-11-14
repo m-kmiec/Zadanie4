ed5194c4b09360fe3b3eef921dfb28e2b588f62d
Michal Kmiec <michal.kmiec01@gmail.com>
2022-11-14 19:45:25 +0100

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,21 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+a
+1app/src/main/java/pl/edu/pb/todoapp/Category.java,4\e\4ec8f6828967205a9bfcd4392999579ce4ed7cc5
+e
+5app/src/main/java/pl/edu/pb/todoapp/MainActivity.java,b\9\b9df7dba3b46d7f90190235284d3bc992977ee7f
+o
+?app/src/main/java/pl/edu/pb/todoapp/SingleFragmentActivity.java,3\c\3cb37fa6b0b09b1d0ba630ae40093e45fe1e8f1c
+]
+-app/src/main/java/pl/edu/pb/todoapp/Task.java,1\7\17f2a9112824e533e728015b44fa57c9f9958d18
+e
+5app/src/main/java/pl/edu/pb/todoapp/TaskFragment.java,f\c\fc449efe8b70dad74a8c54ec0499d3ea1ca767af
+i
+9app/src/main/java/pl/edu/pb/todoapp/TaskListActivity.java,8\5\8523ccd8b65cb43c35a9e86a7d291f8bea723198
---
+++ app/src/main/java/pl/edu/pb/todoapp/TaskStorage.java
@@ -0,0 +1,50 @@
+package pl.edu.pb.todoapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TaskStorage {
+    private static final TaskStorage taskStorage = new TaskStorage();
+    private final List<Task> tasks;
+
+    public static TaskStorage getInstance() {
+        return taskStorage;
+    }
+
+    private TaskStorage() {
+        tasks = new ArrayList<>();
+        for (int i = 1; i <= 150; i++) {
+            Task task = new Task();
+
+            task.setName("Pilne zadanie numer " + i);
+            task.setDone(i % 3 == 0);
+
+            if (i % 3 == 0) {
+                task.setCategory(Category.STUDIES);
+            } else {
+                task.setCategory(Category.HOME);
+            }
+
+            tasks.add(task);
+        }
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task getTask(UUID taskId) {
+        for (Task task : tasks) {
+            if (task.getId().equals(taskId)) {
+                return task;
+            }
+        }
+        return null;
+    }
+}
+
